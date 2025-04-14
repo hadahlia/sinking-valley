d47b658e8d25b800c123fffcf9b380d544be55b0
魔女ラジ <57945008+hadahlia@users.noreply.github.com>
2025-04-14 19:48:50 -0400

    level controller script, playing dungeon music

--- scripts/global/GlobalAudio.gd
+++ scripts/global/GlobalAudio.gd
@@ -6,14 +6,23 @@ const INVENTORY_OPEN : AudioStream = preload("res://sound/sfx/inv_open_ui28.wav"
 const INVENTORY_CLOSE : AudioStream = preload("res://sound/sfx/inv_close_UI4.wav")
 
 # MUSIC
-
+const DUNGEON_THEME : AudioStream = preload("res://sound/music/dungeon.ogg")
 
 # you can call this global script, and pass in the global sfx consts ^^
 func play_sound(stream: AudioStream):
 	var inst = AudioStreamPlayer.new()
 	inst.stream = stream
-	inst.volume_db -= 3
+	inst.volume_db -= 5
 	inst.finished.connect(remove_audio.bind(inst))
+	add_child(inst)
+	inst.play()
+
+func play_music(stream:AudioStream):
+	var inst = AudioStreamPlayer.new()
+	inst.stream = stream
+	inst.volume_db -= 8
+	#inst.volume_db -= 3
+	#inst.finished.connect(remove_audio.bind(inst))
 	add_child(inst)
 	inst.play()
 
---
+++ scripts/level_controller.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+#@export_node_path("Player") var player_ref
+#@onready var player_scene: Player = $"../player_scene"
+@onready var player_scene: Player = $player_scene
+
+
+@onready var temple_spawnpoint : Marker3D = get_tree().get_first_node_in_group("PlayerSpawnTemple")
+var from_temple_spawnpoint : Marker3D #unused, for now
+@onready var island_spawnpoint : Marker3D = get_tree().get_first_node_in_group("PlayerSpawnIsland")
+
+enum ELocations { ISLAND = 0, DUNGEON = 1}
+var location = ELocations.DUNGEON
+
+func _ready() -> void:
+	
+	#player_ref = get_tree().get_first_node_in_group("Player")
+	#player_scene.ready.connect(check_location)
+	check_location()
+
+func check_location() -> void:
+	match location:
+		0:
+			spawn_entity(player_scene,island_spawnpoint)
+		1:
+			GlobalAudio.play_music(GlobalAudio.DUNGEON_THEME)
+			spawn_entity(player_scene,temple_spawnpoint)
+
+func spawn_entity(entity_: Player, point: Marker3D) -> void:
+	entity_.global_position = point.global_position
+	print("YAYYYYYY")
